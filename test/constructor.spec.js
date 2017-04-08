ad02e29acc4a93d360864ca50706bbc2379a18e6
Yu Yongwoo <uyu423@gmail.com>
2017-04-09 04:35:31 +0900

    add contructor.spec.js cases

--- test/constructor.spec.js
+++ test/constructor.spec.js
@@ -10,6 +10,7 @@ const ct = '2017-04-10T00:00:00Z';
 const ctForm = '2017-04-10 00:00:00';
 const ctKST = '2017-04-10 09:00:00';
 const ctNow = new Date();
+const dateCustom = new Date('2017-04-10');
 
 describe('Constructor Tests', () => {
   it(`new DateTimeOffset(${ct}) expect instaceof DateTimeOffset`, () => {
@@ -23,6 +24,18 @@ describe('Constructor Tests', () => {
   it(`new DateTimeOffset(${ctNow}) expect instaceof DateTimeOffset`, () => {
     const t = new DateTimeOffset(ctNow);
     expect(t).to.be.an.instanceof(DateTimeOffset);
+  });
+  it(`new DateTimeOffset(${dateCustom}) expect instaceof DateTimeOffset`, () => {
+    const t = new DateTimeOffset(dateCustom);
+    expect(t).to.be.an.instanceof(DateTimeOffset);
+  });
+  it(`new DateTimeOffset(${dateCustom}) expect toString(undefined, 'UTC') 2017-04-10 00:00:00`, () => {
+    const t = new DateTimeOffset(dateCustom);
+    expect(t.toString(undefined, 'UTC')).to.be.equal('2017-04-10 00:00:00');
+  });
+  it(`new DateTimeOffset(${dateCustom}) expect toString(undefined, 'KST') 2017-04-10 09:00:00`, () => {
+    const t = new DateTimeOffset(dateCustom);
+    expect(t.toString(undefined, 'KST')).to.be.equal('2017-04-10 09:00:00');
   });
   it(`new DateTimeOffset(${ctForm}, { format: 'YYYY-MM-DD HH:mm:ss' }) expect instaceof DateTimeOffset`, () => {
     const form = 'YYYY-MM-DD HH:mm:ss';
