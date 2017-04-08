c31dd507464258f6c7cd18dceff410962ecaffdb
Yu Yongwoo <uyu423@gmail.com>
2017-04-09 05:04:33 +0900

    add some timezone consts

--- timezone-consts.js
+++ timezone-consts.js
@@ -1,10 +1,21 @@
 const timezone = {
   UTC: 'Atlantic/Reykjavik',
   GMT: 'Atlantic/Reykjavik',
-  'Atlantic/Reykjavik': 'Atlantic/Reykjavik',
+  EDT: 'America/New_York',
+  PDT: 'America/Vancouver',
   KST: 'Asia/Seoul',
-  'Asia/Seoul': 'Asia/Seoul',
+  JST: 'Asia/Tokyo',
+  CST: 'Asia/Shanghai',
+  'UTC-4': 'America/New_York',
+  'UTC-7': 'America/Vancouver',
+  'UTC+8': 'Asia/Shanghai',
   'UTC+9': 'Asia/Seoul',
+  'Atlantic/Reykjavik': 'Atlantic/Reykjavik',
+  'America/New_York': 'America/New_York',
+  'America/Vancouver': 'America/Vancouver',
+  'Asia/Seoul': 'Asia/Seoul',
+  'Asia/Tokyo': 'Asia/Tokyo',
+  'Asia/Shanghai': 'Asia/Shanghai',
 };
 
 export default timezone;
